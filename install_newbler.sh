8b5ff46e5ee3a3826dc4ae5b27c8766b3242b981
GuillaumeGAUTREAU <ggautrea@genoscope.cns.fr>
2015-08-12 17:17:41 +0200

    Update install_newbler.sh

--- install_newbler.sh
+++ install_newbler.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #Script for docker
 
-tar -xzf /bin/newbler.tgz
+tar -xzf /home/bin/newbler.tgz
 cd /home/bin/DataAnalysis*/packages/
 for file in gsSeqTools*x86_64.rpm gsNewbler*x86_64.rpm ;do rpm2cpio $file | cpio -idmv ; done
 export PATH=$PATH:$PWD/opt/454/apps/mapper/bin/
